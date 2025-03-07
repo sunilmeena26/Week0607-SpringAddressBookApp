e668b2dad837ce80028fc7f943878967a2444476
sunilmeena26 <sunilmeena641085@gmail.com>
2025-03-07 15:15:12 +0530

    [Sunil Meena] Add Address entity class with Lombok annotations to define person details like fullName, address, city, state, zipCode, and phoneNumber.

--- AddressBookApp/src/main/java/com/bridgelabz/addressbook/entity/Address.java
+++ AddressBookApp/src/main/java/com/bridgelabz/addressbook/entity/Address.java
@@ -2,6 +2,7 @@ package com.bridgelabz.addressbook.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "AddressBook")
 //Create a class to define the person details
 public class Address {
     //Create a private variable to store person record fullName, address, city, state, zipCode, and phoneNumber

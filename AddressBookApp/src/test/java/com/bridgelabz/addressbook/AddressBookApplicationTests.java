35d00ccd573d3a01b07a301e8649ffd0e4225db3
sunilmeena26 <sunilmeena641085@gmail.com>
2025-03-08 01:47:38 +0530

    [Sunil Meena] Add Section2-UC3-AbilityOfServicesLayerToStoreAddressBookData branch to implemented Services Layer to store AddressBook data in memory as a List, with support for GET, GET by ID, POST, PUT (update by ID), and DELETE operations, to be persisted in DB later.

--- AddressBookApp/src/test/java/com/bridgelabz/addressbook/AddressBookApplicationTests.java
+++ AddressBookApp/src/test/java/com/bridgelabz/addressbook/AddressBookApplicationTests.java
@@ -3,7 +3,7 @@ package com.bridgelabz.addressbook;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//Create a Test class AddressBookApplicationTests for performing Testing
+//Create a Test class AddressBookApplicationTests for performing Testing on AddressBookApplication
 @SpringBootTest
 class AddressBookApplicationTests {
 
